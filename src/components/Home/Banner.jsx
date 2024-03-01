099a62359dcf586648eb18a98e97b2b0742a8496
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-01 15:50:15 +0530

    ca-12

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -53,6 +53,54 @@ export default function Banner({ className }) {
                 </div>
               </div>
             </div>
+            <div className="banner-card xl:flex xl:space-x-[30px] xl:h-[600px]  mb-[30px]">
+              <div data-aos="fade-right" className="xl:w-[740px] w-full h-full">
+                <a to="/single-product">
+                  <picture>
+                    <source
+                      media="(min-width:1025px)"
+                      srcSet={`${
+                        import.meta.env.VITE_PUBLIC_URL
+                      }/src/assets/images/banner-1.png`}
+                    />
+                    <img
+                      src={`${
+                        import.meta.env.VITE_PUBLIC_URL
+                      }/src/assets/images/banner-1.2.png`}
+                      alt=""
+                      className="w-full max-w-full h-auto object-cover"
+                    />
+                  </picture>
+                </a>
+              </div>
+              <div
+                data-aos="fade-left"
+                className="flex-1 flex xl:flex-col flex-row  xl:space-y-[30px] h-full"
+              >
+                <div className="w-full xl:h-1/2">
+                  <a to="/single-product">
+                    <img
+                      src={`${
+                        import.meta.env.VITE_PUBLIC_URL
+                      }/src/assets/images/banner-2.png`}
+                      alt=""
+                      className="w-full h-full"
+                    />
+                  </a>
+                </div>
+                <div className="w-full xl:h-1/2">
+                  <a to="/single-product">
+                    <img
+                      src={`${
+                        import.meta.env.VITE_PUBLIC_URL
+                      }/src/assets/images/banner-3.png`}
+                      alt=""
+                      className="w-full h-full"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
             <div
               data-aos="fade-up"
               className="best-services w-full bg-white flex flex-col space-y-10 lg:space-y-0 lg:flex-row lg:justify-between lg:items-center lg:h-[110px] px-10 lg:py-0 py-10"
