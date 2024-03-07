8d869225b40553fa8659cba53f8e21552be9cb61
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-07 12:29:28 +0530

    inside middlebar profilebuttons is deleted

--- src/components/Headers/Middlebar.jsx
+++ src/components/Headers/Middlebar.jsx
@@ -1,5 +1,3 @@
-import Cart from "../Cart";
-import Star from "../svg/Star";
 import SearchBox from "../SearchBox";
 
 export default function Middlebar({ className, type }) {
@@ -46,65 +44,6 @@ export default function Middlebar({ className, type }) {
             </div>
             <div className="w-[517px] h-[44px]">
               <SearchBox type={type} className="search-com" />
-            </div>
-            <div className="flex space-x-6 items-center">
-              <div className="compaire relative">
-                <a to="/products-compaire">
-                  <span>
-                    <Star />
-                  </span>
-                </a>
-                <span
-                  className={`w-[18px] h-[18px] rounded-full  absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] ${
-                    type === 3 ? "bg-qh3-blue text-white" : "bg-qyellow"
-                  }`}
-                >
-                  2
-                </span>
-              </div>
-              <div className="favorite relative">
-                <a to="/wishlist">
-                  <span>
-                    <Star />
-                  </span>
-                </a>
-                <span
-                  className={`w-[18px] h-[18px] rounded-full  absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] ${
-                    type === 3 ? "bg-qh3-blue text-white" : "bg-qyellow"
-                  }`}
-                >
-                  1
-                </span>
-              </div>
-              <div className="cart-wrapper group relative py-4">
-                <div className="cart relative cursor-pointer">
-                  <a to="/cart">
-                    <span>
-                      <Star />
-                    </span>
-                  </a>
-                  <span
-                    className={`w-[18px] h-[18px] rounded-full  absolute -top-2.5 -right-2.5 flex justify-center items-center text-[9px] ${
-                      type === 3 ? "bg-qh3-blue text-white" : "bg-qyellow"
-                    }`}
-                  >
-                    15
-                  </span>
-                </div>
-                {/* <div className="fixed left-0 top-0 w-full h-full z-40"></div> */}
-                {/* hidden group-hover:block" */}
-                <Cart
-                  type={type}
-                  className="absolute -right-[45px] top-11 z-50 hidden group-hover:block"
-                />
-              </div>
-              <div>
-                <a to="/profile">
-                  <span>
-                    <Star />
-                  </span>
-                </a>
-              </div>
             </div>
           </div>
         </div>
