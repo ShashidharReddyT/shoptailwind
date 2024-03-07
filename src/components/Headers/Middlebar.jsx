a4fc5287bad2f66b1807063efef5384485a11df4
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-07 11:52:20 +0530

    inside the middlebar deleted the logo

--- src/components/Headers/Middlebar.jsx
+++ src/components/Headers/Middlebar.jsx
@@ -8,42 +8,6 @@ export default function Middlebar({ className, type }) {
       <div className="container-x mx-auto h-full">
         <div className="relative h-full">
           <div className="flex justify-between items-center h-full">
-            <div>
-              {type === 3 ? (
-                <a to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/src/assets/images/logo-3.svg`}
-                    alt="logo"
-                  />
-                </a>
-              ) : type === 4 ? (
-                <a to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/src/assets/images/logo-4.svg`}
-                    alt="logo"
-                  />
-                </a>
-              ) : (
-                <a to="/">
-                  <img
-                    width="152"
-                    height="36"
-                    src={`${
-                      import.meta.env.VITE_PUBLIC_URL
-                    }/src/assets/images/logo.svg`}
-                    alt="logo"
-                  />
-                </a>
-              )}
-            </div>
             <div className="w-[517px] h-[44px]">
               <SearchBox type={type} className="search-com" />
             </div>
