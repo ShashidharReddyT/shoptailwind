f4efe359bdd037983c089e515e4f375d83503cd7
ShashidharReddyT <117471132+ShashidharReddyT@users.noreply.github.com>
2024-03-07 11:39:02 +0530

    inside topbar topbarnav is  deleted

--- src/components/Headers/TopBar.jsx
+++ src/components/Headers/TopBar.jsx
@@ -11,31 +11,6 @@ export default function TopBar({ className }) {
       >
         <div className="container-x mx-auto h-full">
           <div className="flex justify-between items-center h-full">
-            <div className="topbar-nav">
-              <ul className="flex space-x-6">
-                <li>
-                  <a to="/">
-                    <span className="text-xs leading-6 text-qblack font-500">
-                      Account
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a to="/tracking-order">
-                    <span className="text-xs leading-6 text-qblack font-500">
-                      Track Order
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a to="/faq">
-                    <span className="text-xs leading-6 text-qblack font-500">
-                      Support
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
             <div className="topbar-dropdowns sm:block hidden">
               <div className="flex space-x-6">
                 <div className="country-select flex space-x-1 items-center">
